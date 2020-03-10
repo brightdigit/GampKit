e677a79ad28e11cc883a952fcdb21f3ff42cad41
Leo Dion <leogdion@brightdigit.com>
2020-03-10 07:46:31 -0400

    added debug info

--- Sources/GampKit/Extensions/URLSession.swift
+++ Sources/GampKit/Extensions/URLSession.swift
@@ -5,7 +5,17 @@ import Foundation
 
 extension URLSession: URLSessionable {
   public func dataTask(with request: URLRequest, _ completion: @escaping (Error?) -> Void) -> URLSessionableDataTask {
-    return dataTask(with: request) { _, _, error in
+    return dataTask(with: request) { data, response, error in
+      #if DEBUG
+        let text = data.flatMap { String(data: $0, encoding: .utf8) }
+        let status = response.flatMap { $0 as? HTTPURLResponse }.map { $0.statusCode }
+        if let status = status {
+          debugPrint("Status Code", status)
+        }
+        if let text = text {
+          debugPrint(text)
+        }
+      #endif
       completion(error)
     }
   }
