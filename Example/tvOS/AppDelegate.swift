378537c2d02e423aa1cfce6e884a2d8a299e8ec0
Leo Dion <leogdion@brightdigit.com>
2020-03-11 13:27:17 -0400

    added macOS, iOS, and tvOS examples

--- Example/tvOS/AppDelegate.swift
+++ Example/tvOS/AppDelegate.swift
@@ -5,7 +5,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    // let storyboard = UIStoryboard(name: "Storyboard", bundle: Bundle.main)
+    window.rootViewController = ViewController(nibName: "ViewController", bundle: nil)
+    window.makeKeyAndVisible()
+    self.window = window
     return true
   }
 }
