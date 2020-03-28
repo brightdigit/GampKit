5cec4b65ef6e7d3161922e8f0e4a3bd65ce59ff4
Leo Dion <leogdion@brightdigit.com>
2020-03-28 10:09:25 -0400

    adding validation results

--- Sources/GampKit/Models/AnalyticsValidationProtocol.swift
+++ Sources/GampKit/Models/AnalyticsValidationProtocol.swift
@@ -1,6 +1,34 @@
 public typealias AnalyticsResult = Result<AnalyticsValidationProtocol?, Error>
 
-public protocol AnalyticsValidationProtocol {}
+public protocol AnalyticsValidationHitProtocol {}
+
+public struct AnalyticsHitPath {
+  let method: String
+  let path: String
+  let scheme: String
+}
+
+public enum AnalyticsHitParserMessageType {
+  case info
+  case warn
+  case error
+}
+
+public struct AnalyticsHitParserMessage {
+  let messageType: AnalyticsHitParserMessageType
+  let description: String
+  let parameter: String?
+}
+
+public struct AnalyticsHitParsingResult {
+  let valid: Bool
+  let hit: AnalyticsHitPath
+  let parserMessage: [AnalyticsHitParserMessage]
+}
+
+public protocol AnalyticsValidationProtocol {
+  var hitParsingResult: [AnalyticsHitParsingResult] { get }
+}
 
 public extension AnalyticsResult {
   init(error: Error?) {
