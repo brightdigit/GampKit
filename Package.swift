2d4fd07d18173507d8c48a2927b47a7671328ffe
Leo Dion <leogdion@brightdigit.com>
2021-11-30 09:54:02 -0500

    Version 0.2.0

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.2"), // dev
-    .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
-    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
-    .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
-    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.1"), // dev
-    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0") // dev
+//    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.2"), // dev
+//    .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
+//    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
+//    .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
+//    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.1"), // dev
+//    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0") // dev
   ],
   targets: [
     .target(
